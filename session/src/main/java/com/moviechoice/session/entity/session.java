adaa559c00816b6afcaa96a9547c236d184dcefb
Samlabb <vyalov05@bk.ru>
2026-02-25 02:57:21 +0300

    feat(session): Добавлен репозиторий для сессии

--- session/src/main/java/com/moviechoice/session/entity/session.java
+++ session/src/main/java/com/moviechoice/session/entity/session.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class session {
+public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
---
+++ session/src/main/java/com/moviechoice/session/repository/SessionRepository.java
@@ -0,0 +1,16 @@
+package com.moviechoice.session.repository;
+
+import com.moviechoice.session.entity.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SessionRepository extends JpaRepository<Session, UUID> {
+    //Ищем ссессию по уникальному коду
+    Optional<Session> findByCode(String code);
+
+    //Проверка на наличие сессии с таким кодом
+    boolean existsByCode(String code);
+}
